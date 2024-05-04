cf4b2c99d876e6d1ec6964fd54bdabfa8b9a2df9
Krithika Chandrasekhar <krithika.chandrasekhar@nelkinda.com>
2024-05-04 17:55:35 +0530

    LeapYear program using TDD

---
+++ Kotlin/LeapYear/LeapYear.kt
@@ -0,0 +1,5 @@
+object LeapYear {
+    fun isLeapYear(year: Int): Boolean {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+    }
+}
---
+++ Kotlin/LeapYear/LeapYearTest.kt
@@ -0,0 +1,6 @@
+fun main() {
+    assert(!LeapYear.isLeapYear(2001))
+    assert(LeapYear.isLeapYear(2004))
+    assert(!LeapYear.isLeapYear(2100))
+    assert(LeapYear.isLeapYear(2400))
+}
