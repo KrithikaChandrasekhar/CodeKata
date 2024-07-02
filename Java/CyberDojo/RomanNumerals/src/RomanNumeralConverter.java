41556777093ff91e0769af6d34f624cb87f988e7
KrithikaChandrasekhar <krithikac1994@gmail.com>
2024-07-02 12:09:00 +0530

    Add more test cases

--- Java/CyberDojo/RomanNumerals/src/RomanNumeralConverter.java
+++ Java/CyberDojo/RomanNumerals/src/RomanNumeralConverter.java
@@ -1,6 +1,27 @@
 public class RomanNumeralConverter {
     public static String toRoman(int number) {
         if (number <= 0) throw new IllegalArgumentException("Roman numerals do not have a representation for zero");
-        return "";
+        StringBuilder result = new StringBuilder();
+        if (number >= 10) {
+            result.append("X");
+            number -= 10;
+        }
+        if (number >= 9) {
+            result.append("IX");
+            number -= 9;
+        }
+        if (number >= 5) {
+            result.append("V");
+            number -= 5;
+        }
+        if (number == 4) {
+            result.append("IV");
+            number -= 4;
+        }
+        if (number < 4) {
+            for (int index = 0; index < number; index++)
+                result.append("I");
+        }
+        return result.toString();
     }
 }
--- Java/CyberDojo/RomanNumerals/src/RomanNumeralsConverterTest.java
+++ Java/CyberDojo/RomanNumerals/src/RomanNumeralsConverterTest.java
@@ -10,4 +10,17 @@ public class RomanNumeralsConverterTest {
         assertEquals("Roman numerals do not have a representation for zero", exception.getMessage());
     }
 
+    @Test
+    public void testNumeraltoRomanConversion() {
+        assertEquals("I", RomanNumeralConverter.toRoman(1));
+        assertEquals("II", RomanNumeralConverter.toRoman(2));
+        assertEquals("III", RomanNumeralConverter.toRoman(3));
+        assertEquals("IV", RomanNumeralConverter.toRoman(4));
+        assertEquals("V", RomanNumeralConverter.toRoman(5));
+        assertEquals("VI", RomanNumeralConverter.toRoman(6));
+        assertEquals("IX", RomanNumeralConverter.toRoman(9));
+        assertEquals("X", RomanNumeralConverter.toRoman(10));
+    }
+
+
 }
