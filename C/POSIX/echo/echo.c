8f4191f57236644d340694f6f9c455aa308db4e9
Krithika Chandrasekhar <krithika.chandrasekhar@nelkinda.com>
2024-08-01 15:08:24 +0530

    Implement logic to pass multiple arguments to the echo command

--- C/POSIX/echo/echo.c
+++ C/POSIX/echo/echo.c
@@ -3,7 +3,13 @@
 int main(int argc, char *argv[]) {
     if (argc == 1)
         printf("\n");
-    else
-        printf("%s\n", argv[1]);
+    else {
+        for (int argIndex = 1; argIndex < argc; argIndex++) {
+            printf("%s", argv[argIndex]);
+            if (argIndex < argc - 1)
+                printf (" ");
+        }
+        printf("\n");
+    }
     return 0;
 }
