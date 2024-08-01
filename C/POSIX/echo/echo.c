724c80b14ef73c91d560ab0de9d7039fde3c05b6
Krithika Chandrasekhar <krithika.chandrasekhar@nelkinda.com>
2024-08-01 15:00:52 +0530

    When calling echo with an argument, it prints the same argument back with a new line character

--- C/POSIX/echo/echo.c
+++ C/POSIX/echo/echo.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("\n");
+int main(int argc, char *argv[]) {
+    if (argc == 1)
+        printf("\n");
+    else
+        printf("%s\n", argv[1]);
     return 0;
 }
