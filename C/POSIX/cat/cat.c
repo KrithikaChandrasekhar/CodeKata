16d77db438536303131ee8a0ba5dfc1207b77065
Krithika Chandrasekhar <krithika.chandrasekhar@nelkinda.com>
2024-08-06 15:12:19 +0530

    errno message for No such file or directory

--- C/POSIX/cat/cat.c
+++ C/POSIX/cat/cat.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 
-int main(void) {
+void fileCopy(FILE *file);
+
+int main(int argc, char *argv[]) {
+   const char *programName = argv[0];
+    if (argc == 1)
+        fileCopy(stdin);
+    else {
+        for (int i = 1; i < argc; i++) {
+            char *filename = argv[i];
+            FILE *file = fopen(filename, "r");
+            if (file == NULL) {
+                fprintf(stderr, "%s: %s: %s\n", programName, filename, strerror(errno));
+                continue;
+            }
+            fileCopy(file);
+            fclose(file);
+        }
+    }
+        return 0;
+}
+
+void fileCopy(FILE *file) {
     int c;
-    while ((c = getchar()) != EOF) {
+    while ((c = fgetc(file)) != EOF) {
         putchar(c);
     }
-    return 0;
 }
