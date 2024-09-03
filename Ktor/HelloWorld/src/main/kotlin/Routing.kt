aecee11afdf3fe12dae102edf784eaf97107b1d1
Krithika Chandrasekhar <krithika.chandrasekhar@nelkinda.com>
2024-09-03 11:13:21 +0530

    feat: Handling JSON Serialization

--- Ktor/HelloWorld/src/main/kotlin/Routing.kt
+++ Ktor/HelloWorld/src/main/kotlin/Routing.kt
@@ -1,6 +1,7 @@
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
 
 fun Application.configureRouting() {
     routing {
@@ -11,6 +12,12 @@ fun Application.configureRouting() {
         get("/about") {
             call.respondText("About Us")
         }
+        get("/user") {
+            val user = User(name = "John Doe", age = 30)
+            call.respond(user)
+        }
     }
-
 }
+
+@Serializable
+data class User(val name: String, val age: Int)
