221049cf952717917452c5eca1d700c09e6f4ddf
Krithika Chandrasekhar <krithika.chandrasekhar@nelkinda.com>
2024-09-03 12:51:14 +0530

    fix: Install the Authentication plugin in the test environment to match application setup.

--- Ktor/HelloWorld/src/test/kotlin/ApplicationTest.kt
+++ Ktor/HelloWorld/src/test/kotlin/ApplicationTest.kt
@@ -1,6 +1,8 @@
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -9,6 +11,18 @@ class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
         application {
+            install(Authentication) {
+                basic(name = "John Doe") {
+                    realm = "Ktor Server"
+                    validate { credentials ->
+                        if (credentials.name == "John Doe" && credentials.password == "password") {
+                            UserIdPrincipal(credentials.name)
+                        } else {
+                            null
+                        }
+                    }
+                }
+            }
             configureRouting()
         }
         client.get("/").apply {
